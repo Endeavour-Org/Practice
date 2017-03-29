f1ea6ae5a21911a678da7f81bc6fefae794ef351
Whitney Lampkin <wlampkin.davis@hotmail.com>
2017-03-29 11:54:26 -0500

    Adding Listings 1-16 through 1-24 (3/29/17)

---
+++ Listing1-24_OrderedParallelQuery/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Listing1_24_OrderedParallelQuery
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var numbers = Enumerable.Range(0, 10);
+            var parallelResult = numbers.AsParallel()
+                .Where(i => i % 2 == 0)
+                .ToArray();
+
+            foreach (var item in parallelResult)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
