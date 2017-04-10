2b49aee0cf6f290d257bdad33e0e61bae2173031
Whitney Lampkin <wlampkin.davis@hotmail.com>
2017-04-10 17:11:07 -0500

    More Practice Stuff

---
+++ AlphabetChekc/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphabetChekc
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            {
+
+                int n = Convert.ToInt32(Console.ReadLine());
+                string[] arr_temp = Console.ReadLine().Split(' ');
+                int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+
+                int sum = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+
+                    sum += arr[i];
+                };
+                Console.Write(sum);
+                Console.ReadLine();
+            }
+        }
+    }
+}
+  
---
+++ Listing1-40_UsingInterlocked/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listing1_40_UsingInterlocked
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
